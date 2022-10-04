6dc3e83e258c1af9669a0da1b489b3e57703dc3a
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 04:46:49 +0000

    Adding numeric_owner as keyword arguement

--- gateway/gateway/data/bundles/quandl.py
+++ gateway/gateway/data/bundles/quandl.py
@@ -355,7 +355,7 @@ def quantopian_quandl_bundle(environ,
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
         
-            tar.extractall(path, members, numeric_owner) 
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
             
         
         safe_extract(tar, output_dir)
--- gateway/tests/test_examples.py
+++ gateway/tests/test_examples.py
@@ -64,7 +64,7 @@ class ExamplesTests(WithTmpDir, GatewayTestCase):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(tar, cls.tmpdir.path)
